5e08015c9df354a8ad320752cf26a6665ff51d9f
David Zaletanski <david.zaletanski@gmail.com>
2019-04-01 20:32:40 -0500

    Added npm package

---
+++ index.js
@@ -0,0 +1,8 @@
+/***
+ *          thalamo
+ * *************************
+ * File:        /index.js
+ * Author:      David Zaletanski <david.zaletanski@gmail.com> (https://dzale.net/)
+ * Description: The main entrypoint for the application.
+ *
+***/
